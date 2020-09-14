a25a60e02e1caf132447510e74b7d37a18b43b63
Arvind Subramaniam <37202614+ArvindSubramaniam@users.noreply.github.com>
2020-09-14 23:13:17 +0530

    Update Semantics.cs

--- Semantics.cs
+++ Semantics.cs
@@ -4,9 +4,17 @@ namespace ProseTutorial
 {
     public static class Semantics
     {
-        public static int Substring(int[] v, int start, int end)
+        public static int Add(int[] v, int start, int end)
         {
             return start + end;
+        }
+        public static int multiply(int[] v, int start, int end)
+        {
+            return start*end;
+        }
+        public static int divide(int[] v, int start, int end)
+        {
+            return end!=0?start/end:0;
         }
 
         public static int? AbsPos(int[] v, int k)
